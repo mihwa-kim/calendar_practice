f5711d9a83b5015dde99e1ec1bf9beba0bfc1202
Mihwa_Kim <64059691+mihwa-kim@users.noreply.github.com>
2023-02-15 21:13:51 +1100

    Date picker practice

--- calendar_practice/ContentView.swift
+++ calendar_practice/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var reservationdate = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section(header: Text("Date & Time")) {
+                    DatePicker("Choose a date", selection: $reservationdate)
+                    
+                }}
+            .navigationTitle("Search events")
         }
-        .padding()
     }
 }
 
