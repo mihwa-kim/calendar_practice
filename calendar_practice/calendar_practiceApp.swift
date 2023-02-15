24e04f563ea54a2a610aa9ba9aa0bdb1adcc3010
Mihwa_Kim <64059691+mihwa-kim@users.noreply.github.com>
2023-02-15 19:45:55 +1100

    Initial Commit

---
+++ calendar_practice/calendar_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendar_practiceApp.swift
+//  calendar_practice
+//
+//  Created by 김미화 on 2023/02/15.
+//
+
+import SwiftUI
+
+@main
+struct calendar_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
